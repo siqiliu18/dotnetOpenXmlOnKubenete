2e6d39c6a3b69099fef877fae096ddee792bc932
Siqi Liu <siqiliu18@gmail.com>
2020-04-16 14:09:42 -0700

    create helper class in a different file

--- SiqiSecondPptMerge/Program.cs
+++ SiqiSecondPptMerge/Program.cs
@@ -4,19 +4,26 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using DocumentFormat.OpenXml.Packaging;
+using SiqiSecondPptMerge.Properties;
 using Drawing = DocumentFormat.OpenXml.Drawing;
 
 namespace SiqiSecondPptMerge
 {
     class MainClass
     {
-        private static readonly string BaseDatasetsRelativePath = @"../../../Data";
-        private static readonly string TemplatePath = GetAbsolutePath(BaseDatasetsRelativePath + "/template.pptx");
-        private static readonly string OutputPath = GetAbsolutePath(BaseDatasetsRelativePath + "/output.pptx");
-        private static readonly string ImagePath = GetAbsolutePath(BaseDatasetsRelativePath + "/testingImage.png");
+        //private static readonly string BaseDatasetsRelativePath = @"../../../Data";
+        //private static readonly string TemplatePath = GetAbsolutePath(BaseDatasetsRelativePath + "/template.pptx");
+        //private static readonly string OutputPath = GetAbsolutePath(BaseDatasetsRelativePath + "/output.pptx");
+        //private static readonly string ImagePath = GetAbsolutePath(BaseDatasetsRelativePath + "/testingImage.png");
 
         public static void Main(string[] args)
         {
+            OpenXmlHelper openXmlProcessor = new OpenXmlHelper(@"../../../Data");
+
+            string TemplatePath = openXmlProcessor.GetAbsolutePath("/template.pptx");
+            string OutputPath = openXmlProcessor.GetAbsolutePath("/output.pptx");
+            string ImagePath = openXmlProcessor.GetAbsolutePath("/testingImage.png");
+
             File.Copy(TemplatePath, OutputPath, true);
 
             using (PresentationDocument presentationDoc = PresentationDocument.Open(OutputPath, true))
@@ -41,12 +48,12 @@ namespace SiqiSecondPptMerge
                         m.Position = 0;
                         imagePart.FeedData(m);
 
-                        SwapPhoto(contentSlidePart, imgId);
+                        openXmlProcessor.SwapPhoto(contentSlidePart, imgId);
                     }
                 }
 
                 // Step 3: Replace text matched by the key
-                SwapPlaceholderText(contentSlidePart, "{{Title}}", "Testing Title for IBM");
+                openXmlProcessor.SwapPlaceholderText(contentSlidePart, "{{Title}}", "Testing Title for IBM");
 
                 // Step 4: Fill out multi value fields by table
                 var tupleList = new List<(string category, string model, string price)>
@@ -64,9 +71,9 @@ namespace SiqiSecondPptMerge
                 {
                     Drawing.TableRow tr = new Drawing.TableRow();
                     tr.Height = 100;
-                    tr.Append(CreateTextCell(row.category));
-                    tr.Append(CreateTextCell(row.model));
-                    tr.Append(CreateTextCell(row.price));
+                    tr.Append(openXmlProcessor.CreateTextCell(row.category));
+                    tr.Append(openXmlProcessor.CreateTextCell(row.model));
+                    tr.Append(openXmlProcessor.CreateTextCell(row.price));
                     tbl.Append(tr);
                 }
 
@@ -75,47 +82,47 @@ namespace SiqiSecondPptMerge
             }
         }
 
-        static Drawing.TableCell CreateTextCell(string text)
-        {
-            Drawing.TableCell tc = new Drawing.TableCell(
-                                new Drawing.TextBody(
-                                    new Drawing.BodyProperties(),
-                                new Drawing.Paragraph(
-                                    new Drawing.Run(
-                                        new Drawing.Text(text)))),
-                                new Drawing.TableCellProperties());
+        //static Drawing.TableCell CreateTextCell(string text)
+        //{
+        //    Drawing.TableCell tc = new Drawing.TableCell(
+        //                        new Drawing.TextBody(
+        //                            new Drawing.BodyProperties(),
+        //                        new Drawing.Paragraph(
+        //                            new Drawing.Run(
+        //                                new Drawing.Text(text)))),
+        //                        new Drawing.TableCellProperties());
 
-            return tc;
-        }
+        //    return tc;
+        //}
 
-        static void SwapPhoto(SlidePart slidePart, string imgId)
-        {
-            Drawing.Blip blip = slidePart.Slide.Descendants<Drawing.Blip>().First();
-            blip.Embed = imgId;
-            slidePart.Slide.Save();
-        }
+        //static void SwapPhoto(SlidePart slidePart, string imgId)
+        //{
+        //    Drawing.Blip blip = slidePart.Slide.Descendants<Drawing.Blip>().First();
+        //    blip.Embed = imgId;
+        //    slidePart.Slide.Save();
+        //}
 
-        static void SwapPlaceholderText(SlidePart slidePart, string placeholder, string value)
-        {
-            //Find and get all the placeholder text locations 
-            List<Drawing.Text> textList = slidePart.Slide.Descendants<Drawing.Text>()
-                                        .Where(t => t.Text.Equals(placeholder)).ToList();
+        //static void SwapPlaceholderText(SlidePart slidePart, string placeholder, string value)
+        //{
+        //    //Find and get all the placeholder text locations 
+        //    List<Drawing.Text> textList = slidePart.Slide.Descendants<Drawing.Text>()
+        //                                .Where(t => t.Text.Equals(placeholder)).ToList();
 
-            //Swap the placeholder text with other text
-            foreach (Drawing.Text text in textList)
-            {
-                text.Text = value;
-            }
-        }
+        //    //Swap the placeholder text with other text
+        //    foreach (Drawing.Text text in textList)
+        //    {
+        //        text.Text = value;
+        //    }
+        //}
 
-        public static string GetAbsolutePath(string relativePath)
-        {
-            FileInfo _dataRoot = new FileInfo(typeof(MainClass).Assembly.Location);
-            string assemblyFolderPath = _dataRoot.Directory.FullName;
+        //public static string GetAbsolutePath(string relativePath)
+        //{
+        //    FileInfo _dataRoot = new FileInfo(typeof(MainClass).Assembly.Location);
+        //    string assemblyFolderPath = _dataRoot.Directory.FullName;
 
-            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+        //    string fullPath = Path.Combine(assemblyFolderPath, relativePath);
 
-            return fullPath;
-        }
+        //    return fullPath;
+        //}
     }
 }
---
+++ SiqiSecondPptMerge/Properties/OpenXmlHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using Drawing = DocumentFormat.OpenXml.Drawing;
+
+namespace SiqiSecondPptMerge.Properties
+{
+    public class OpenXmlHelper
+    {
+        private string baseDatasetsRelativePath;
+        //public string templatePath;
+        //public string outputPath;
+        //public string imagePath;
+
+        //default constructer
+        public OpenXmlHelper(string inputBasePath)
+        {
+            baseDatasetsRelativePath = inputBasePath;
+        }
+
+        public Drawing.TableCell CreateTextCell(string text)
+        {
+            Drawing.TableCell tc = new Drawing.TableCell(
+                                new Drawing.TextBody(
+                                    new Drawing.BodyProperties(),
+                                new Drawing.Paragraph(
+                                    new Drawing.Run(
+                                        new Drawing.Text(text)))),
+                                new Drawing.TableCellProperties());
+
+            return tc;
+        }
+
+        public void SwapPhoto(SlidePart slidePart, string imgId)
+        {
+            Drawing.Blip blip = slidePart.Slide.Descendants<Drawing.Blip>().First();
+            blip.Embed = imgId;
+            slidePart.Slide.Save();
+        }
+
+        public void SwapPlaceholderText(SlidePart slidePart, string placeholder, string value)
+        {
+            //Find and get all the placeholder text locations 
+            List<Drawing.Text> textList = slidePart.Slide.Descendants<Drawing.Text>()
+                                        .Where(t => t.Text.Equals(placeholder)).ToList();
+
+            //Swap the placeholder text with other text
+            foreach (Drawing.Text text in textList)
+            {
+                text.Text = value;
+            }
+        }
+
+        public string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(MainClass).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, baseDatasetsRelativePath + relativePath);
+
+            return fullPath;
+        }
+    }
+}
