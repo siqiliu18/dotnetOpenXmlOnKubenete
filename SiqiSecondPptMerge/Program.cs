85734869c8feaf840d12c3e39147f45ab9dfa922
Siqi Liu <siqiliu18@gmail.com>
2020-04-16 14:10:28 -0700

    remove unused comments

--- SiqiSecondPptMerge/Program.cs
+++ SiqiSecondPptMerge/Program.cs
@@ -11,11 +11,6 @@ namespace SiqiSecondPptMerge
 {
     class MainClass
     {
-        //private static readonly string BaseDatasetsRelativePath = @"../../../Data";
-        //private static readonly string TemplatePath = GetAbsolutePath(BaseDatasetsRelativePath + "/template.pptx");
-        //private static readonly string OutputPath = GetAbsolutePath(BaseDatasetsRelativePath + "/output.pptx");
-        //private static readonly string ImagePath = GetAbsolutePath(BaseDatasetsRelativePath + "/testingImage.png");
-
         public static void Main(string[] args)
         {
             OpenXmlHelper openXmlProcessor = new OpenXmlHelper(@"../../../Data");
@@ -81,48 +76,5 @@ namespace SiqiSecondPptMerge
                 presPart.Presentation.Save();
             }
         }
-
-        //static Drawing.TableCell CreateTextCell(string text)
-        //{
-        //    Drawing.TableCell tc = new Drawing.TableCell(
-        //                        new Drawing.TextBody(
-        //                            new Drawing.BodyProperties(),
-        //                        new Drawing.Paragraph(
-        //                            new Drawing.Run(
-        //                                new Drawing.Text(text)))),
-        //                        new Drawing.TableCellProperties());
-
-        //    return tc;
-        //}
-
-        //static void SwapPhoto(SlidePart slidePart, string imgId)
-        //{
-        //    Drawing.Blip blip = slidePart.Slide.Descendants<Drawing.Blip>().First();
-        //    blip.Embed = imgId;
-        //    slidePart.Slide.Save();
-        //}
-
-        //static void SwapPlaceholderText(SlidePart slidePart, string placeholder, string value)
-        //{
-        //    //Find and get all the placeholder text locations 
-        //    List<Drawing.Text> textList = slidePart.Slide.Descendants<Drawing.Text>()
-        //                                .Where(t => t.Text.Equals(placeholder)).ToList();
-
-        //    //Swap the placeholder text with other text
-        //    foreach (Drawing.Text text in textList)
-        //    {
-        //        text.Text = value;
-        //    }
-        //}
-
-        //public static string GetAbsolutePath(string relativePath)
-        //{
-        //    FileInfo _dataRoot = new FileInfo(typeof(MainClass).Assembly.Location);
-        //    string assemblyFolderPath = _dataRoot.Directory.FullName;
-
-        //    string fullPath = Path.Combine(assemblyFolderPath, relativePath);
-
-        //    return fullPath;
-        //}
     }
 }
